ac3e4d8a24b46b66aab7c5db8d3817f52245eb48
Amine Tahiri Alaoui <atahiri-@e1r4p15.1337.ma>
2025-10-16 10:06:47 +0100

    Changed: renamed global test variables

--- castrum.h
+++ castrum.h
@@ -6,7 +6,7 @@
 /*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 07:56:44 by atahiri-          #+#    #+#             */
-/*   Updated: 2025/10/16 10:01:32 by atahiri-         ###   ########.fr       */
+/*   Updated: 2025/10/16 10:05:56 by atahiri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 
 #define PRINT_TYPE(exp) \
 	_Generic((exp), \
-		int:			sprintf(buf + idx, "%d", (int)exp), \
-		unsigned int:	sprintf(buf + idx, "%u", (unsigned int)exp), \
-		long:			sprintf(buf + idx, "%ld", (long)exp), \
-		unsigned long:	sprintf(buf + idx, "%lu", (unsigned long)exp), \
-		char:			sprintf(buf + idx, "%c", (char)exp), \
-		unsigned char:	sprintf(buf + idx, "%hhx", (unsigned char)exp), \
-		void *:			sprintf(buf + idx, "%p", (void *)(long)exp) \
+		int:			sprintf(g_msg_buf + g_buf_idx, "%d", (int)exp), \
+		unsigned int:	sprintf(g_msg_buf + g_buf_idx, "%u", (unsigned int)exp), \
+		long:			sprintf(g_msg_buf + g_buf_idx, "%ld", (long)exp), \
+		unsigned long:	sprintf(g_msg_buf + g_buf_idx, "%lu", (unsigned long)exp), \
+		char:			sprintf(g_msg_buf + g_buf_idx, "%c", (char)exp), \
+		unsigned char:	sprintf(g_msg_buf + g_buf_idx, "%hhx", (unsigned char)exp), \
+		void *:			sprintf(g_msg_buf + g_buf_idx, "%p", (void *)(long)exp) \
 	)
 
 #define SUCCESS_MSG(func) \
@@ -33,93 +33,93 @@
 	printf("%s at line %d: \x1b[41mFAIL\x1b[0m\n", #func, __LINE__);
 
 int g_test_failed;
-char buf[1024];
-unsigned long idx;
+char g_msg_buf[2048];
+unsigned long g_buf_idx;
 
 #define RUN_TEST(func) \
 	g_test_failed = 0; \
-	idx = 0; \
+	g_buf_idx = 0; \
 	func(); \
 	if (g_test_failed) { \
 		FAIL_MSG(func) \
-		printf("%s", buf); \
+		printf("%s", g_msg_buf); \
 	} else \
 		SUCCESS_MSG(func)
 
 #define ASSERT_TRUE(exp) \
 	if (!exp) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #exp); \
-		idx += PRINT_TYPE(exp); \
-		idx += sprintf(buf + idx, " to be true!\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #exp); \
+		g_buf_idx += PRINT_TYPE(exp); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be true!\n"); \
 		g_test_failed++; \
 	}
 
 
 #define ASSERT_FALSE(exp) \
 	if (exp) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #exp); \
-		idx += PRINT_TYPE(exp); \
-		idx += sprintf(buf + idx, " to be false!\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #exp); \
+		g_buf_idx += PRINT_TYPE(exp); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be false!\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_EQ(lhs, rhs) \
 	if (lhs != rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to equal \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to equal \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_NE(lhs, rhs) \
 	if (lhs == rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to not equal \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to not equal \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_GT(lhs, rhs) \
 	if (lhs <= rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to be greater than \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be greater than \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_GE(lhs, rhs) \
 	if (lhs < rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to be greater or equal to \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be greater or equal to \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_LT(lhs, rhs) \
 	if (lhs >= rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to be less than \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be less than \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
 #define ASSERT_LE(lhs, rhs) \
 	if (lhs > rhs) { \
-		idx += sprintf(buf + idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
-		idx += PRINT_TYPE(lhs); \
-		idx += sprintf(buf + idx, " to be less or equal to \x1b[1m%s\x1b[0m: ", #rhs); \
-		idx += PRINT_TYPE(rhs); \
-		idx += sprintf(buf + idx, "\n"); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\t-> expected \x1b[1m%s\x1b[0m: ", #lhs); \
+		g_buf_idx += PRINT_TYPE(lhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, " to be less or equal to \x1b[1m%s\x1b[0m: ", #rhs); \
+		g_buf_idx += PRINT_TYPE(rhs); \
+		g_buf_idx += sprintf(g_msg_buf + g_buf_idx, "\n"); \
 		g_test_failed++; \
 	}
 
